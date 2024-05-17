1ca467b5ae9fdb1044934677de2abc72b09f67db
Tushar Gandhi <104029815+tushargandhi77@users.noreply.github.com>
2024-05-17 19:33:47 +0530

    Update Cart.js

--- Frontend/src/screens/Cart.js
+++ Frontend/src/screens/Cart.js
@@ -18,7 +18,7 @@ export default function Cart() {
 
     const handleCheckOut = async()=>{
         let userEmail = localStorage.getItem("userEmail")
-        let response = await fetch("http://localhost:5000/api/orderData",{
+        let response = await fetch("wefoodbackend-git-main-tushargandhi77s-projects.vercel.app/api/orderData",{
             method:'POST', 
             headers: {
                 'Content-Type': 'application/json'
