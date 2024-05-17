8740a1f407292425a45cfe848bdd48520ab13266
Tushar Gandhi <104029815+tushargandhi77@users.noreply.github.com>
2024-05-17 20:18:19 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -19,7 +19,7 @@ app.use(cors(corsOptions));
 
 
 app.use((req,res,next)=>{
-    res.setHeader("Access-Control-Allow-Origin","http://localhost:3000")
+    res.setHeader("Access-Control-Allow-Origin","https://we-food-swart.vercel.app")
     res.header(
         "Access-Control-Allow-Headers",
         "Origin,X-Requested-with,Content-Type,Accept"
