5deb3d965ec89c6c283fd23b780a85b273004e87
Tushar Gandhi <104029815+tushargandhi77@users.noreply.github.com>
2024-05-18 10:49:00 +0530

    Update MyOrder.js

--- Frontend/src/screens/MyOrder.js
+++ Frontend/src/screens/MyOrder.js
@@ -8,7 +8,7 @@ export default function MyOrder() {
 
     const fetchMyOrder = async () => {
         console.log(localStorage.getItem('userEmail'))
-        await fetch("wefoodbackend.vercel.app/api/myOrderData", {
+        await fetch("htpps://wefoodbackend.vercel.app/api/myOrderData", {
             // credentials: 'include',
             // Origin:"http://localhost:3000/login",
             method: 'POST',
