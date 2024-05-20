e249459fd359eec0a3cb2b458fe07360890c7149
Tushar Gandhi <104029815+tushargandhi77@users.noreply.github.com>
2024-05-20 15:37:45 +0530

    Update db.js

--- Backend/db.js
+++ Backend/db.js
@@ -1,36 +1,13 @@
 const mongoose = require('mongoose');
 const mongoURI = 'mongodb+srv://gofood:gofood123@cluster0.v1mhr0d.mongodb.net/Gofoodmern?retryWrites=true&w=majority';
 
-// Function to load data from the database
-const loadData = async () => {
-    try {
-        const db = mongoose.connection.db;
-        
-        const foodItemsCollection = db.collection("food_items");
-        const foodItemsData = await foodItemsCollection.find({}).toArray();
-        
-        const foodCategoryCollection = db.collection("foodCategory");
-        const foodCategoryData = await foodCategoryCollection.find({}).toArray();
-        
-        global.food_items = foodItemsData;
-        global.foodCategory = foodCategoryData;
-
-        console.log("Data loaded successfully");
-    } catch (err) {
-        console.error("Error loading data:", err);
-    }
-};
-
-// Connect to the database and load data
-const mongoDB = async () => {
+const connectDB = async () => {
     try {
         await mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true });
         console.log('Connected to MongoDB');
-
-        await loadData();
     } catch (err) {
         console.error("Error connecting to MongoDB:", err);
     }
 };
 
-module.exports = mongoDB;
+module.exports = connectDB;
